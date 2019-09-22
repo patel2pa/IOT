22d8163d5e65914042a37140c3d075544080abea
Parth Patel <parthpp1996@gmail.com>
2019-09-21 21:51:22 -0400

    Update StepCounter.py

--- StepCounter.py
+++ StepCounter.py
@@ -7,18 +7,18 @@ from scipy.ndimage.filters import gaussian_filter1d
 from scipy import signal
 import statistics 
 
-data = pd.read_csv('14_steps.csv')
- 
+data = pd.read_csv('48_steps.csv')
+
 x = []
- 
+
 for d in range(len(data['x'])):
     #print(data['x'][d])
     mag = (data['x'][d])**2 + (data['y'][d])**2 + (data['z'][d])**2
     x.append(math.sqrt(mag))
- 
- 
- 
-ysmoothed = gaussian_filter1d(x, sigma = 4)
+
+
+
+ysmoothed = gaussian_filter1d(x, sigma = 2)
 
 
 def StartingandStopingPointFinder(SmoothedArray):
@@ -73,15 +73,44 @@ def StartingandStopingPointFinder(SmoothedArray):
     print(slopChanges)
     print(starting)
     print(ending)
-    return SmoothedArray[starting*5:ending*5]
+    return SmoothedArray[starting*5:ending*5], slopChanges
+      
+#plt.plot(gaussian_filter1d(x, sigma = 4))  
+a,b = StartingandStopingPointFinder(x)
+#plt.plot(gaussian_filter1d(b, sigma = 4))
+#plt.plot(gaussian_filter1d(b,sigma = 4))       
+
+def getSlop(inputArray):
+    array_ = []
+    for t in range(len(inputArray)):
+        if (inputArray[t] - inputArray[t-1] > 0):
+            array_.append(1)
+        else:
+            array_.append(-1)
+    return array_
+
+def CountTheOnes(inputArray):
+
+    CountOfOnes = 0
+    iterationPlace = 0
+    consecutiveOnes  = 0
     
-        
-            
-    
-  
-plt.plot(gaussian_filter1d(x, sigma = 4))  
-b = StartandStopValues(x)
-plt.plot(gaussian_filter1d(b, sigma = 4))
-        
-            
+    while iterationPlace < len(inputArray):
+        if inputArray[iterationPlace] == 1:
+            consecutiveOnes = consecutiveOnes + 1
+            iterationPlace = iterationPlace + 1
+        elif inputArray[iterationPlace] == -1:
+            if consecutiveOnes >= 6:
+                CountOfOnes = CountOfOnes + 1
+            consecutiveOnes = 0
+            iterationPlace = iterationPlace + 1
+    return CountOfOnes
+   
+q = getSlop(gaussian_filter1d(a, sigma = 4))
+plt.plot(q)
+plt.plot(gaussian_filter1d(a, sigma = 4))
 plt.show()
+u = CountTheOnes(q)
+
+
+ 
